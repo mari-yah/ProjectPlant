f04ada895cd0f92594e9843a6e865033fb266c13
Mariyah <mariyah.dahodwala@gmail.com>
2024-12-11 12:35:32 +0530

    Added login page

--- plant/plant/urls.py
+++ plant/plant/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from plantcare import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
 ]
